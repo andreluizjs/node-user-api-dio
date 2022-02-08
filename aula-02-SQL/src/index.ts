584e756b3afee9611f944856e69de2ff97d52a8c
drelluiz <andrejsluiz@gmail.com>
2022-02-08 17:46:56 -0300

    Conexão com banco de dados e Erro genérico

--- aula-02-SQL/src/index.ts
+++ aula-02-SQL/src/index.ts
@@ -1,10 +1,11 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import errorHandler from './middlewares/error-handler.middleware';
 import statusRoute from './routes/status.route';
 import usersRoute from './routes/users.route';
 
 const app = express();
 
-// Configurações da Aplicação
+// Configurações da aplicação
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,7 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(statusRoute);
 app.use(usersRoute);
 
-// Inicialização o Servidor
+// Configuração dos Handlers de Erro
+app.use(errorHandler);
+
+// Inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
 });
