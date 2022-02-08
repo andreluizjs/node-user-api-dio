f6ed68176cce1cd23adc6d7a709bd227a3957b85
drelluiz <andrejsluiz@gmail.com>
2022-02-08 17:34:40 -0300

    Finalizando aula 01

--- aula-01/src/index.ts
+++ aula-01/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Executei");
+import express, { Request, Response, NextFunction } from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
+
+const app = express();
+
+// Configurações da Aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configurações de Rotas
+app.use(statusRoute);
+app.use(usersRoute);
+
+// Inicialização o Servidor
+app.listen(3000, () => {
+    console.log('Aplicação executando na porta 3000!');
+});
